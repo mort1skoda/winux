512b5c41af68000d0c55f8493fe7960037f776b5
mort1skoda <mort1skoda@gmail.com>
2023-01-20 10:30:26 +0100

    2023_01_20__10_30_25

--- .aliases.bat
+++ .aliases.bat
@@ -1,9 +1,14 @@
 @echo off
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Author: Morten Håkestad <mort1skoda@gmail.com>
 :: Github: https://github.com/mort1skoda/winuxin10.git
-:: In this batch file I am trying to mimic my linux bash aliases.
+::
+:: .aliases.bat try to make cmd mimic bash.
+:: Make my shell work under Windows and Linux the same.
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-::--- alias navigate the DOS shell ------------{{{
+
+::--- alias make cmd mimic bash ---------------{{{
 doskey  ..=cd ..
 ::doskey  cp=copy
 ::doskey  mv=ren
@@ -12,33 +17,43 @@ doskey   sa=%HOME%\.aliases.bat
 ::doskey   sb=bashrc.bat
 ::doskey   sp=profile.bat
 doskey   l=dir /a /o
-doskey  la=dir /ah
-::---------------------------------------------}}}
+::doskey  la=dir /ah
+::--------------------------------------}}}
+
 
 ::--- alias edit with vim --------------------{{{
 :: set a variable e.g: Editor=gvim
 doskey   v=gvim $*
-doskey ,ea=C:\Users\Monica\gVim.lnk aliases.bat
-doskey ,eb=gVim bashrc.bat
-doskey ,ep=gVim profile.bat
+doskey ,ea=gvim %WINUX_HOME%\.aliases.bat
+::doskey ,eb=gVim bashrc.bat
+::doskey ,ep=gVim profile.bat
 ::doskey ,ev=gVim _vimrc
 doskey   c=type $*
-::doskey   q=quit.bat
+doskey   q=exit
+::--------------------------------------}}}
 
-::---------------------------------------------}}}
 
-::--- alias git -----------{{{
+::--- alias git ------------------------------{{{
 doskey   gc=gitCommitTime.bat
 doskey   gp=git push
 doskey   gr=git remote -v
 doskey   gs=git status
 doskey ,egi=gvim %HOME%\.gitignore
-::-------------------------}}}
 
-::--- alias show all my aliases ----------{{{
-::doskey /macros:all
+:: git step by step:
+doskey g0 = git pull
+doskey g1 = git status
+doskey g2 = git remote -v
+doskey g3 = git add $*
+doskey g4 = git status
+doskey g5 = gitCommitTime.bat
+doskey g6 = git push
+::--------------------------------------}}}
+
+
+::--- alias list -----------------------------{{{
 doskey /macros
-::----------------------------------------}}}
+::--------------------------------------}}}
 
 
 
