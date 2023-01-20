6bde561f810bf2f9ba6ab249e9637a8be2d8807e
mort1skoda <mort1skoda@gmail.com>
2023-01-20 15:46:23 +0100

    2023-01-20 15:45:50

--- .aliases.sh
+++ .aliases.sh
@@ -4,12 +4,16 @@ echo "in: .aliases.sh"
 
 
 #### aliases for bash ##################################{{{
+alias ..='cd ..'
 alias l='ls -la --color --group-directories-first'
+alias c='cat '
+
+
 alias v='gvim'
 alias sa='source .aliases.sh'
 alias sb='source .bashrc'
 alias sp='source .bash_profile'
-alias ,ea='gvim .aliases.sh &'
+alias ,ea='gvim .aliases.sh && source .aliases.sh'
 
 alias g='grep -i --color '
 alias ag='alias | grep -i --color '
@@ -25,7 +29,7 @@ alias g1s='git status && git remote -v && ag git'
 alias g2a='git add '
 alias g3s='git status && ag git'
 
-DATE=$(DATE +"%Y-%m-%d %H:%M:%S")
+DATE=$(date +"%Y-%m-%d %H:%M:%S")
 echo "DateString \$DATE for git commit -m:" $DATE
 echo $DATE
 alias g4c='git commit -m "$DATE" '
